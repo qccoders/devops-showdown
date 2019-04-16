400d3010f8bbd8755a667063e581d097f5b77d2e
JP Dillingham <jpdillingham@gmail.com>
2019-04-16 18:34:40 -0500

    add jest, update npm test script

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
-console.log("hello world!")
-
-let data = [
-    {order_id: 10,kot_id: 1,price: 200,quantity: 1},
-    {order_id: 10,kot_id: 1,price: 300,quantity: 1},
-    {order_id: 11,kot_id: 2,price: 400,quantity: 3}
-]
-
-data = data.reduce((dict, v) => { 
-    let { order_id, ...rest } = v
-    dict[order_id] = dict[order_id] === undefined ? [ rest ] : dict[order_id].concat(rest)
-    return dict
-}, {})
-
-console.log("result", data);
+console.log("hello world!");
